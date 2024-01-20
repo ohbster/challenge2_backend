508072e346e9dbe67f94aa4b0cbc80bcf090d323
ohbster <ohbster@protonmail.com>
2024-01-20 09:06:49 -0600

    before remain

--- main.tf
+++ main.tf
@@ -17,9 +17,47 @@ locals {
   common_tags = merge(var.common_tags, { mff_id = random_uuid.uuid.result })
 }
 
-module "s3" {
-  source = "../../Terraform/myfinalform/aws/modules/s3"
+# module "s3" {
+#   source = "../../Terraform/myfinalform/aws/modules/s3"
+#   name = "ohbster-project2-s3"
+#   tags = var.common_tags
 
+# }
+resource "aws_s3_bucket" "bucket" {
+  bucket = "ohbster-project2"
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_public_block" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  # ignore_public_acls      = false
+  # restrict_public_buckets = false
+}
+
+
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.bucket.id
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "policy" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "${aws_s3_bucket.bucket.arn}/*"
+        }
+    ]
+})
 }
 
 # module "vpc-network" {
